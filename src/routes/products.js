f4726054f1f73535013b1946a19140b8fcb7ab20
Risano <muhammadrisano@gmail.com>
2022-01-27 14:40:52 +0700

    add redis

--- src/routes/products.js
+++ src/routes/products.js
@@ -4,21 +4,6 @@ const productController = require('../controllers/products')
 const upload = require('../middlewares/multer')
 const auth = require('../middlewares/auth')
 const redisCache = require('../middlewares/redis')
-// const mymiddle = (req, res, next)=>{
-//   // res.send('hello ini middle saya')
-//   console.log('saya mengolah data terlebih dahulu');
-//   // next()
-// }
-// const auth = (req, res, next)=>{
-//   const authValid = true
-//   if(authValid){
-//     next()
-//   }else{
-//     res.json({
-//       message: 'auth failed'
-//     })
-//   }
-// }
 router
   .get('/', auth.verifyAccess, redisCache.hitCacheAllProduct, productController.getAllProduct)
   .get('/:idsaya', auth.verifyAccess, redisCache.hitCacheProductId, productController.getProductById)
